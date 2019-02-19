48e30bc656de9d0a63a1749bc376bb60f5d70680
mani <andrei.bajeu@gmail.com>
2019-02-19 15:55:21 +0100

    add examplem of stagger

---
+++ examples/stagger/README.md
@@ -0,0 +1,10 @@
+# Stagger
+
+## What does it do?
+
+
+
+## Why is it useful?
+
+Instead of animating a group of items all at once, we can recreate this same effect by staggering the animations. When each item's animation is incrementally delayed, they appear as individuals, but still move collectively as a whole.
+
---
+++ examples/stagger/script.js
@@ -0,0 +1,19 @@
+var t1 = new TimelineMax({paused: true});
+
+
+t1.to(".menu", 1.5, {
+     top: "0%",
+     ease: Expo.easeInOut,
+     delay: -0.5
+});
+
+t1.staggerFrom(".menu ul li", 1.5, {x: -200, opacity: 0, ease:Expo.easeOut}, 0.3);
+
+t1.reverse();
+$(document).on("click", ".toggle-btn", function() {
+     t1.reversed(!t1.reversed());
+});
+$(document).on("click", "a", function() {
+     t1.reversed(!t1.reversed());
+});
+
