d822b84153a18d58bc3deb4cad6fa6fcefeaa4d4
Victor Baquero Wihlborg <jag@vbw.se>
2019-02-21 09:07:10 +0100

    Add opacity condition and comments

--- examples/draw-text/main.js
+++ examples/draw-text/main.js
@@ -2,7 +2,7 @@
 const duration = 5;
 let tween;
 
-// Graphics to animate
+// Save references to graphics to animate
 const path = document.querySelector(".greensock-text");
 const track = document.querySelector(".track-completion");
 const percent = document.querySelector(".track-percentage");
@@ -13,7 +13,7 @@ const animation = {
   pathLength: path.getTotalLength()
 };
 
-// Tween control buttons
+// Save references to tween control buttons
 const reverse = document.querySelector(".reverse");
 const pause = document.querySelector(".pause");
 const play = document.querySelector(".play");
@@ -21,6 +21,7 @@ const scrubber = document.querySelector(".scrubber");
 
 // Easing
 const easingButtons = document.querySelectorAll(".easing button");
+// Array of different easing functions to choose from
 const easingFuncs = [
   Linear.easeNone,
   Strong.easeInOut,
@@ -36,15 +37,24 @@ const easingFuncs = [
     clamp: true
   })
 ];
+// set first easing func as default
 let easing = easingFuncs[0];
 
 // function run during tween to update ui
 const update = () => {
   const completion = (animation.length / animation.pathLength) * 100;
+  // Update position and sizes of graphics
   path.style.strokeDasharray = `${animation.length} ${animation.pathLength}`;
   track.style.width = `${completion}%`;
   percent.innerHTML = `${Math.round(tween.progress() * 100)}%`;
   scrubber.value = tween.progress() * 100;
+
+  // Set opacity to 0 to avoid visible dot when the animation is at its start position
+  if (animation.length > 0) {
+    path.style.opacity = 1;
+  } else {
+    path.style.opacity = 0;
+  }
 };
 
 // Init tween
@@ -67,13 +77,12 @@ pause.addEventListener("click", () => {
 reverse.addEventListener("click", () => {
   tween.reverse();
 });
-scrubber.addEventListener("change", e => {
-  console.log(e.target.value);
+scrubber.addEventListener("input", e => {
   tween.pause();
   tween.progress(e.target.value / 100);
 });
 
-// bind easing buttons
+// Bind easing buttons
 easingButtons.forEach((button, i) => {
   button.addEventListener("click", () => {
     tween.updateTo({ ease: easingFuncs[i] });
